69e05eee84cc687154b22f150de83e330b4a933e
aup9qmb <aup9qmb@virginia.edu>
2024-12-11 16:36:41 -0500

    arrows move

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 # imports
+import os
 import sys
-
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
+# i will not reinvent the wheel i will not reinvent the wheel i will n
 import pygame.image
 
 # ddr imports
-from src.utils import *
+from src.window import *
 import src.entities as ent
 
-# initialize engine, create window
-pygame.init()
-
-WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-WINDOW.fill(WHITE)
-ICON = pygame.image.load(IMGPATH + "R_arrow_active.png")
-pygame.display.set_caption("DDR-Clone")
-pygame.display.set_icon(ICON)
 
 ##########################################
+
+
 background = pygame.image.load(IMGPATH + 'bkg.png')
 fore1 = pygame.image.load(IMGPATH + 'bottom_bar.png')
 fore2 = pygame.image.load(IMGPATH + 'top_bar.png')
@@ -26,21 +22,34 @@ bgl = {
     1: {'image': fore1, 'pos': (0, SCREEN_HEIGHT - fore1.get_rect().bottom)},
     2: {'image': fore2, 'pos': (0, 0)}
 }
+
+receptors = []
 arrows = []
 
 
 # Main function
 def main():
-    print(bgl[1]['pos'])
+    # Left receptor (arrow), player 1
+    receptors.append(ent.receptor('L', coords['L']))
+    receptors.append(ent.receptor('D', coords['D']))
+    receptors.append(ent.receptor('U', coords['U']))
+    receptors.append(ent.receptor('R', coords['R']))
+
+    arrows.append(ent.arrow('L', (coords['L'][0], 400), 1))
+
     while True:
+        # update background assets
+        # blit(source, dest, area) -> Rect
         for i in bgl:
             WINDOW.blit(bgl[i]['image'], bgl[i]['pos'])
-
+        for r in receptors:
+            WINDOW.blit(r.image, r.pos)
         for a in arrows:
-            WINDOW.blit(background, a.pos, a.pos)
+            WINDOW.blit(a.image, a.pos)
 
-        ent.arrow('U').draw(WINDOW)
+        arrows[0].move(coords['L'][1])
 
+        # exit if closed
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
