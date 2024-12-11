411e580778e7c1f16a512ed11e72f1f0428a0b78
aup9qmb <aup9qmb@virginia.edu>
2024-12-11 16:35:23 -0500

    arrow/receptor class

--- src/entities.py
+++ src/entities.py
@@ -1,32 +1,55 @@
 from src.utils import *
+from src.window import WINDOW
 
 #########################################
 
 
 class arrow(pygame.sprite.Sprite):
 
-    def __init__(self, direction):
+    def __init__(self, d, xycoords, speed):
         super().__init__()
-        texture = f'res/images/{direction}_arrow_active.png'
+        texture = f'res/images/{d}_arrow_active.png'
         self.image = pygame.image.load(texture)
+        # speed needs to be calc'd from bpm
+        self.speed = speed
         self.rect = self.image.get_rect()
+        self.pos = self.image.get_rect().move(xycoords)
 
-        # center object in window
-        self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+        WINDOW.blit(self.image, self.pos)
+
+    def move(self, max_y):
+        self.pos.top -= self.speed
+        if self.pos.top < 0:
+            self.pos.top = 0
+        if self.pos.top < max_y:
+            self.pos.top = max_y
+
+    def get_area(self):
+        area = (int(self.rect.left), int(self.rect.top),
+                int(self.rect.width), int(self.rect.height))
+        return area
 
     def retexture(self, texture):
         self.image = pygame.image.load(texture)
         self.rect = self.image.get_rect()
 
-    def move(self):
-        self.rect.move_ip(0, 10)
-        if (self.rect.bottom > 600):
-            self.rect.top = 0
-            self.rect.center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+class receptor(pygame.sprite.Sprite):
 
+    def __init__(self, d, xycoords):
+        super().__init__()
+        texture = f'res/images/{d}_arrow.png'
+        self.image = pygame.image.load(texture)
+        self.rect = self.image.get_rect()
+        self.rect.center = xycoords
+        self.pos = self.rect.center
+        self.area = (self.rect.left, self.rect.top, self.rect.width, self.rect.height)
+
+        WINDOW.blit(self.image, self.pos)
+
+    def retexture(self, texture):
+        self.image = pygame.image.load(texture)
+        self.rect = self.image.get_rect()
 
 ########################################
 
